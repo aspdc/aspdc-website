2d18189d6bde8123742d98d9e06f540045a45583
Devvrat1010 <rathoddevvratsingh777@gmail.com>
2023-10-26 20:31:02 +0530

    push...Pa

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,18 +1,19 @@
 import Link from "next/link"
 export default function Navbar(props) {
     const style=`hover:${props.hoverColour} duration-200`
+    const aah="text-white"
     return(
         <div className={`gap-96 justify-between w-fit translate-y-[30%] m-auto flex item-center`}>
                 <div className="logo text-[40px] text-[#00CA08] animate-pulse font-regular ">
                     {"</>"}
                 </div>
-                <div className={`text ${props.textColour}  flex gap-10 items-center font-light`}>
-                    <Link href="/" className={{style}} id="home" >Home</Link>
-                    <Link href="/pages/events" className={{style}} id="events">Events</Link>
-                    <Link href="/pages/resources" className={{style}} id="resources"  >Resources</Link>
-                    <Link href="/pages/gallery" className={{style}} id="gallery"  >Gallery</Link>
-                    <Link href="/pages/projects" className={{style}} id="projects"  >Projects</Link>
-                    <Link href="/pages/leaderboard" className={{style}} id="leaderboard" >Leaderboard</Link>
+                <div className={`text ${props.textColour}  flex gap-10 items-center font-light text-[18px] `}>
+                    <Link href="/" className={style} id="home" >Home</Link>
+                    <Link href="/pages/events" className={style} id="events">Events</Link>
+                    <Link href="/pages/resources" className={style} id="resources"  >Resources</Link>
+                    <Link href="/pages/gallery" className={style} id="gallery"  >Gallery</Link>
+                    <Link href="/pages/projects" className={style} id="projects"  >Projects</Link>
+                    <Link href="/pages/leaderboard" className={style} id="leaderboard" >Leaderboard</Link>
                 </div>
 
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "hero-pattern":"url('https://i.ibb.co/gDc2wCw/Coding.jpg')",
       },
     },
   },
