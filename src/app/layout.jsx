71d377f0e0c740d12363a8a0a8545791cc734321
Devvrat1010 <rathoddevvratsingh777@gmail.com>
2023-11-01 16:12:17 +0530

    sdijf

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import './globals.css'
 import Navbar from './components/Navbar'
 import { ClerkProvider } from '@clerk/nextjs'
 import { Inter } from 'next/font/google'
+import NextTopLoader from 'nextjs-toploader';
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,6 +17,10 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
             <html lang="en" >
                 <body className={`w-screen h-screen p-0 m-0 overflow-x-hidden scale-100 ${inter.className}`}>
+                    <NextTopLoader
+                        color='#42b883'
+                    />
+
                     {children}
                 </body>
             </html>
